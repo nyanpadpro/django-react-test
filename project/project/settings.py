2c691366deece4f129c38c98625b2a6b7d1e1c0e
nyanpadpro <nyanpadpro@gmail.com>
2018-07-19 22:24:43 +0000

    変更

--- project/project/settings.py
+++ project/project/settings.py
@@ -131,4 +131,18 @@ if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
 
-ALLOWED_HOSTS = ['*']
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
